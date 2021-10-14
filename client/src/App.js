6449de232bc4cd839cd8261e1eea05bad4720c38
Amine <vipamine33@gmail.com>
2021-10-14 18:41:27 +0100

    setup react

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="landing">
+     <h1>DevNetwork</h1>
+     </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+                
